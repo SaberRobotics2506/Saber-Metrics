688eb0f7ab63cba70614fa7315d9a81a11d5f11d
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-01-22 09:47:33 -0600

    The App actually builds itself!

--- app/src/androidTest/java/com/example/frc_scouting/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/frc_scouting/ExampleInstrumentedTest.kt
@@ -10,9 +10,9 @@ import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
