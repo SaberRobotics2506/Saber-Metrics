188740f32e59a1207e80329a4fce6665f50c1f10
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-01-22 10:59:21 -0600

    Add SQL Queries

---
+++ sql/Create Match Master.sql
@@ -0,0 +1,21 @@
+CREATE TABLE MatchMaster(
+Team# Int,
+Match# Int,
+ScoutedBy VarChar (100),
+Regional VarChar (100),
+AutoLowGoalMake Int,
+AutoLowGoalMiss Int,
+AutoHighGoalMake Int,
+AutoHighGoalMiss Int,
+Taxi Int,
+TelopLowGoalMake Int,
+TelopLowGoalMiss Int,
+TelopHighGoalMake Int,
+TelopHighGoalMiss Int,
+DefensePlays Int,
+EndGameStatus Int,
+EndGameSuccess Int,
+AllianceScore Int,
+WinLossTie VarChar(1), 
+Comments VarChar (500)
+);
