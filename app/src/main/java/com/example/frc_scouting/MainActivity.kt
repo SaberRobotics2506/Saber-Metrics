bc64ff4e5691284658a9c73660eed5cac17b4f66
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-01-26 18:48:57 -0600

    Remove LoginActivity from the build.

--- app/src/main/java/com/example/frc_scouting/MainActivity.kt
+++ app/src/main/java/com/example/frc_scouting/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.frc_scouting
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            println("This looks like it is working I think!$view");
-        }
     }
 }
