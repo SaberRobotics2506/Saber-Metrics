b83f0f2f2cf598dc3c91e4b11f59c927877a0a83
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-02-02 19:04:25 -0600

    Button Click Detection & Text Field

--- app/src/main/java/com/example/frc_scouting/MainActivity.kt
+++ app/src/main/java/com/example/frc_scouting/MainActivity.kt
@@ -3,11 +3,27 @@ package com.example.frc_scouting
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+import android.widget.Button;
+import android.widget.Toast;
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //setSupportActionBar(findViewById(R.id.toolbar))
+        //setSupportActionBar(findViewById(R.id.toolbar)) This line is commented because with it the app didn't build correctly... Since idk what this does I keep it
+
+        // get reference to form submission button
+        val sendButton = findViewById<Button>(R.id.Send)
+
+        // set on-click listener
+        sendButton.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            onFormSubmission()
+        }
+    }
+
+    fun onFormSubmission() {
+        Toast.makeText(this@MainActivity, "The data you have entered is being sent to the server.\nDO NOT TURN OFF YOUR DEVICE OR DISABLE BLUETOOTH.", Toast.LENGTH_LONG).show()
     }
 }
