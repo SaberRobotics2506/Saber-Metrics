2b07faf9afaddc5a96a62cd2d06abb1269eeb82a
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-02-03 18:34:43 -0600

    Create new activity

---
+++ app/src/main/java/com/example/frc_scouting/BluetoothActivity.kt
@@ -0,0 +1,11 @@
+package com.example.frc_scouting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BluetoothActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+    }
+}
