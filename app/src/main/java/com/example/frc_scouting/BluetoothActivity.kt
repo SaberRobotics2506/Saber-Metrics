b44130cca5f4599e30df8ba1c436bcaa5deb567e
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-02-05 11:57:24 -0600

    Add Submit Button and Bluetooth GUI

--- app/src/main/java/com/example/frc_scouting/BluetoothActivity.kt
+++ app/src/main/java/com/example/frc_scouting/BluetoothActivity.kt
@@ -2,10 +2,13 @@ package com.example.frc_scouting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class BluetoothActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bluetooth)
+
+        Toast.makeText(this@BluetoothActivity, "⚠ DO NOT DISABLE BLUETOOTH OR EXIT THIS ACTIVITY ⚠", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/frc_scouting/DataEntryActivity.kt
+++ app/src/main/java/com/example/frc_scouting/DataEntryActivity.kt
@@ -1,5 +1,6 @@
 package com.example.frc_scouting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,18 +12,31 @@ class DataEntryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_data_entry)
 
         whatsThisListeners()
+        submissionListener()
 
     }
 
     private fun whatsThisListeners()
     {
-        // get reference to form submission button
+        // get reference to what's this buttons
         val teamNumberHelp = findViewById<Button>(R.id.teamNumberHelp)
         val matchNumberHelp = findViewById<Button>(R.id.matchNumberHelp)
 
-        // set on-click listener
+        // listen for on-click and run Toast
         teamNumberHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the team number of the team you are currently scouting. It should be printed on the bumper guard of their robot.", Toast.LENGTH_LONG).show() }
         matchNumberHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the match number for the match you are currently scouting. The match number should be visible on the scoreboard or main display.", Toast.LENGTH_LONG).show() }
 
+    }
+
+    private fun submissionListener()
+    {
+        //Get reference to form submission button
+        val submitButton = findViewById<Button>(R.id.submit)
+
+        //Switch to BlueTooth Activity
+        submitButton.setOnClickListener(){
+            val switchActivityIntent = Intent(this, BluetoothActivity::class.java)
+            startActivity(switchActivityIntent)
+        }
     }
 }
