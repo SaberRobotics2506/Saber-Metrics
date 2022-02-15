b7a301aa414e36f9ecc7f82f8de65bdd29e31acc
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-02-15 16:53:51 -0600

    Add more data validation fields

--- app/src/main/java/com/ibxcodecat/frc_scouting/DataValidator.java
+++ app/src/main/java/com/ibxcodecat/frc_scouting/DataValidator.java
@@ -3,14 +3,15 @@ package com.ibxcodecat.frc_scouting;
 public class DataValidator {
 
     final int TOTAL_MATCHES = 95;
+    final int SHORT_STRING_LEN = 100;
 
     int[] validTeams = { 93, 167, 876, 1675, 1732, 2052, 2175, 2227, 2239, 2264, 2501, 2503, 2506, 2526, 2531, 2861, 2977, 3058, 3102, 3122, 3134, 3197, 3275, 3276, 3291, 3294, 3740, 4009, 4181, 4230, 4238, 4511, 4607, 4656, 4674, 4728, 4741, 5125, 5143, 5253, 5348, 5464, 5653, 5690, 5826, 5847, 6047, 6146, 6160, 6217, 6318, 6574, 7048, 7068, 7311, 7797, 7893, 8836 };
 
     //Checks data in DataEntryActivity, returns -1 for pass, and a positive int for the problematic field
-    public enum DataError { TeamNumberError, MatchNumberError, NONE };
+    public enum DataError { TeamNumberError, MatchNumberError, ScoutedByErrror, NONE };
     private static DataError dataError;
 
-    public DataError CheckData(String teamNumber, String matchNumber)
+    public DataError CheckData(String teamNumber, String matchNumber/*, String scoutedBy*/)
     {
         if(!ValidateTeamNumber(teamNumber))
         {
@@ -21,6 +22,11 @@ public class DataValidator {
         {
             return dataError.MatchNumberError;
         }
+
+        /*if(!ValidateScoutedBy(scoutedBy))
+        {
+            return dataError.ScoutedByErrror;
+        }*/
 
         return dataError.NONE;
     }
@@ -62,6 +68,16 @@ public class DataValidator {
         }
 
         if(match < TOTAL_MATCHES)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean ValidateScoutedBy(String scoutedBy)
+    {
+        if(scoutedBy.length() < SHORT_STRING_LEN)
         {
             return true;
         }
