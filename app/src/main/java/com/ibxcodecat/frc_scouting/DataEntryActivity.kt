1ac9c63a3eb9dbe3c2b769ce1ac026d6f47a5cec
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-02-14 20:46:29 -0600

    Gson no workie

--- app/src/main/java/com/ibxcodecat/frc_scouting/DataEntryActivity.kt
+++ app/src/main/java/com/ibxcodecat/frc_scouting/DataEntryActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.frc_scouting.R
 
 class DataEntryActivity : AppCompatActivity() {
+
+    //var gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
