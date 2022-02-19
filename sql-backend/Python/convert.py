1760f897d127f7b1fada3209e88dec8f4a6848aa
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-02-19 11:01:09 -0600

    Return list of data dicts

--- sql-backend/Python/convert.py
+++ sql-backend/Python/convert.py
@@ -14,15 +14,18 @@ def ReadAllData():
 	cwd = os.getcwd() #Get current working directory (cwd)
 	files = os.listdir(cwd) #Read all files from cwd and add to file list
 	
-	scoutData = "" #Create scoutData variable
+	data_list = [] #Declaring a list of data dicts
 	
 	for file in files: #Loop through all files in the file list
 		if(searchForFileType in file): #If the filetype is of ".scout"...
 			with open(file) as json_data: #Read json data of file into "json_data"
 				data = json.load(json_data) #Save loaded json data to "data"
-				print(data)
-				
-ReadAllData()
+				data_list.append(data)
+	
+	return data_list
+
+data = ReadAllData()
+print(data)
 
 #with open(jsonFile) as json_data:
 #	data = json.load(json_data)
