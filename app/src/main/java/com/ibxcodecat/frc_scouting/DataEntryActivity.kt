2d4eff00d1300cf2501c6bdac31a3fb95af8dd45
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-02-20 16:41:46 -0600

    Change some strings

--- app/src/main/java/com/ibxcodecat/frc_scouting/DataEntryActivity.kt
+++ app/src/main/java/com/ibxcodecat/frc_scouting/DataEntryActivity.kt
@@ -91,12 +91,12 @@ class DataEntryActivity : AppCompatActivity() {
         val scoreHelp = findViewById<Button>(R.id.scoreHelp)
 
         // listen for on-click and run Toast
-        teamNumberHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the team number of the team you are currently scouting. It should be printed on the bumper guard of their robot.", Toast.LENGTH_LONG).show() }
-        matchNumberHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the match number for the match you are currently scouting. The match number should be visible on the scoreboard or main display.", Toast.LENGTH_LONG).show() }
+        teamNumberHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the team number of the team you are currently scouting.", Toast.LENGTH_LONG).show() }
+        matchNumberHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the match number for the match you are currently scouting.", Toast.LENGTH_LONG).show() }
         scouterNameHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is your name num nuts!", Toast.LENGTH_LONG).show() }
-        regionalHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the regional you are currently attending. Are you Milwaukee or Duluth?", Toast.LENGTH_LONG).show() }
+        regionalHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "Are you in Milwaukee or Duluth?", Toast.LENGTH_LONG).show() }
         taxiHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "Did the robot drive across the line during autonomous?", Toast.LENGTH_LONG).show() }
-        scoreHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the total aliance score for the robot ou are scouting!", Toast.LENGTH_LONG).show() }
+        scoreHelp.setOnClickListener { Toast.makeText(this@DataEntryActivity, "This is the total alliance score for the robot ou are scouting!", Toast.LENGTH_LONG).show() }
     }
 
     private fun submissionListener()
