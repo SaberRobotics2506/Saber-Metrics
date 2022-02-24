3cca7fa42cd6f0ccdb087f82fe36d2d9cd7c848b
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-02-23 19:29:06 -0600

    Create Code with Code

--- sql-backend/Automation/sqlBuilder.py
+++ sql-backend/Automation/sqlBuilder.py
@@ -3,6 +3,9 @@
 #Imports from python
 import json
 
+#Imports from subprocess
+import subprocess
+
 #Imports from os
 import os
 from os import listdir
@@ -10,6 +13,13 @@ from os import listdir
 #Imports from os.path
 from os.path import isfile
 from os.path import join
+
+####################CONSTANTS####################
+
+BUILDER_OUTPUT_FILE_NAME = "sqlBuilderOutput.sql"
+CURRENT_WORKING_DIRECTORY = os.getcwd() #Get current working directory (cwd) of this file
+SEARCH_FOR_FILETYPE = ".scout" #The filetype to include in the file searches
+TABLE_NAME = "MatchMaster1" #The name of the SQL table we are writing queries for
 
 ####################FUNCTIONS####################
 
@@ -21,14 +31,12 @@ def ReadJSON():
     # EXAMPLE RETURN: [{'firstFileOneInteger': 1, secondFileOneInteger: 2}, {file2Integer: 2}]
     # NOTE: This function will only read the "*.scout" files in the current working directory
 
-	searchForFileType = ".scout" #The filetype to include in the file search
-	cwd = os.getcwd() #Get current working directory (cwd) of this file
-	files = os.listdir(cwd) #Read all files from cwd and add to file list
+	files = os.listdir(CURRENT_WORKING_DIRECTORY) #Read all files from cwd and add to file list
 	
 	data_list = [] #Declaring an empty list of data dictionaries to append to
 	
 	for file in files: #Loop through all files in the file list
-		if(searchForFileType in file): #If the filetype is of ".scout"...
+		if(SEARCH_FOR_FILETYPE in file): #If the filetype is of ".scout"...
 			try:
 				with open(file) as json_data: #Read json data of file into "json_data"
 					data = json.load(json_data) #Save loaded json data to a "data" dictionary
@@ -39,26 +47,39 @@ def ReadJSON():
 	return data_list #Returns a list of dictionaries representing each files' JSON Data
 
 def BuildInsertQueries(data):
-    # This function builds SQL queries from each dictionary KeyValuePair in a list of dictionaries
-    # This function will loop over every JSON dictionary in the list of dictionaries and then loop over Each KeyValuePair in the dictionary
-    # We will then generate an insert querry by taking using the key as the column and the value as the data point
-    # EXAMPLE QUERY: INSERT INTO MatchMaster1 (ScoutedBy)VALUES(John Doe)
+	# This function builds SQL queries from each dictionary KeyValuePair in a list of dictionaries
+	# This function will loop over every JSON dictionary in the list of dictionaries and then loop over Each KeyValuePair in the dictionary
+	# We will then generate an insert querry by taking using the key as the column and the value as the data point
+	# EXAMPLE QUERY: INSERT INTO MatchMaster1 (ScoutedBy)VALUES(John Doe)
     
-    query_list = [] #Define an empty list to store SQL querries in
-    TABLE_NAME = "MatchMaster1" #Store the name of the table to be referenced again later when creating the query
+	query_list = [] #Define an empty list to store SQL querries in
+	TABLE_NAME = "MatchMaster1" #Store the name of the table to be referenced again later when creating the query
 	
-    for json_dictionary in data: #For each JSON dictionary in the data list...
-        for key, value in json_dictionary.items(): #Loop through all keys and values in the dictionary for this JSON file
-            query = "INSERT INTO " + TABLE_NAME #Start constructing this INSERT query using the table name specified
-            query = query + "(" + str(key) + ")VALUES(" + str(value) + ")" #Create an INSERT query for this KeyValuePair
-            query_list.append(query) #Append the query we created to the query_list
+	for json_dictionary in data: #For each JSON dictionary in the data list...
+		for key, value in json_dictionary.items(): #Loop through all keys and values in the dictionary for this JSON file
+			query = "INSERT INTO " + TABLE_NAME #Start constructing this INSERT query using the table name specified
+			query = query + "(" + str(key) + ")VALUES(" + str(value) + ")" #Create an INSERT query for this KeyValuePair
+			query_list.append(query) #Append the query we created to the query_list
             
-    return query_list #Return the list of queries that we have created
-    
+	return query_list #Return the list of queries that we have created
+	
+def WriteQueryFile(query_list):
+	with open(BUILDER_OUTPUT_FILE_NAME, "w+") as builderOutputFile:
+		fileContents = ("--This SQL query file was built with the SQL Builder python script\n\n")
+		
+		for query in query_list:
+			fileContents = fileContents + (str(query) + " --Generated Query\n")
+		
+		builderOutputFile.write(fileContents)
+		
+def RunSQLQuery():
+	cwd = os.getcwd() #Get current working directory (cwd) of this file
+	subprocess.call(CURRENT_WORKING_DIRECTORY + BUILDER_OUTPUT_FILE_NAME)
+	
 ####################MAIN####################
     
 data = ReadJSON()
-print(data)
-
 queries = BuildInsertQueries(data)
-print (queries)
+WriteQueryFile(queries)
+
+#RunSQLQuery()
