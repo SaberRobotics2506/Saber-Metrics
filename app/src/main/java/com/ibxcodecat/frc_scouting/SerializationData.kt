4b9d8af2596415064a382c42c6f8604293a6b49c
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-03-01 19:19:42 -0600

    Update Fields to Match SQL database fields

--- app/src/main/java/com/ibxcodecat/frc_scouting/SerializationData.kt
+++ app/src/main/java/com/ibxcodecat/frc_scouting/SerializationData.kt
@@ -1,27 +1,27 @@
 package com.ibxcodecat.frc_scouting
 
 data class SerializationData (
-    val team: Int = -1,
-    val match: Int = -1,
+    val TeamNumber: Int = -1,
+    val MatchNumber: Int = -1,
 
-    val scouter: String = "Empty Field",
-    val regional: String = "Empty Field",
+    val ScoutedBy: String = "Empty Field",
+    val Regional: String = "Empty Field",
 
-    val taxi: Boolean = false,
+    val Taxi: Boolean = false,
 
-    val allianceScore: Int = -1,
+    val AllianceScore: Int = -1,
 
-    val comments: String = "Empty Field",
+    val Comments: String = "Empty Field",
 
-    val autoLowGoalSuccess: Int = -1,
-    val autoLowGoalFail: Int = -1,
-    val autoHighGoalSuccess: Int = -1,
-    val autoHighGoalFail: Int = -1,
+    val AutoLowGoalMake: Int = -1,
+    val AutoLowGoalMiss: Int = -1,
+    val AutoHighGoalMake: Int = -1,
+    val AutoHighGoalMiss: Int = -1,
 
-    val teleopLowGoalSuccess: Int = -1,
-    val teleopLowGoalFail: Int = -1,
-    val teleopHighGoalSuccess: Int = -1,
-    val teleopHighGoalFail: Int = -1,
+    val TelopLowGoalMake: Int = -1,
+    val TelopLowGoalMiss: Int = -1,
+    val TelopHighGoalMake: Int = -1,
+    val TelopHighGoalMiss: Int = -1,
 
     /*
 
