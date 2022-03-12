a394ca0020215a0ca2915b2ab125fd6c7e556209
IBXCODECAT <47277402+Dom733@users.noreply.github.com>
2022-03-12 12:22:29 -0600

    Counter fragment complete*
    *Not tested, will figure that out later

--- app/src/main/java/com/ibxcodecat/frc_scouting/CounterComponent.kt
+++ app/src/main/java/com/ibxcodecat/frc_scouting/CounterComponent.kt
@@ -17,13 +17,20 @@ class CounterComponent : Fragment() {
         val makeIncBtn: Button = view.findViewById(R.id.makeIncrementBtn)
         val missDecBtn: Button = view.findViewById(R.id.missDecrementBtn)
         val missIncBtn: Button = view.findViewById(R.id.missIncrementBtn)
-        
+
         //Variables to be manipulated (just like my ex in my previous relationship)
         var makeNum: Int = 0
         var missNum: Int = 0
 
+        //Number displays to be manipulated (I won't crack the same joke twice)
+        val missText: TextView = view.findViewById(R.id.missNumText)
+        val makeText: TextView = view.findViewById(R.id.makeNumText)
+
         //Listeners to manipulate the numbers
-        makeDecBtn.setOnClickListener()
+        makeDecBtn.setOnClickListener{makeNum--; if(makeNum < 0) makeNum = 0; if(makeNum == 1) makeText.setText(makeNum.toString() + " bucket") else makeText.setText(makeNum.toString() + " buckets")}
+        makeIncBtn.setOnClickListener{makeNum++; if(makeNum == 1) makeText.setText(makeNum.toString() + " bucket") else makeText.setText(makeNum.toString() + " buckets")}
+        missDecBtn.setOnClickListener{missNum--; if(missNum < 0) missNum = 0; if(missNum == 1) missText.setText(missNum.toString() + "miss") else missText.setText(missNum.toString() + " misses")}
+        missIncBtn.setOnClickListener{missNum++; if(missNum == 1) missText.setText(missNum.toString() + "miss") else missText.setText(missNum.toString() + " misses")}
     }
 
     override fun onCreateView(inflater : LayoutInflater, container : ViewGroup?,
