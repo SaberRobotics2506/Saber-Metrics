6ed401c8db81c23510d886af95270e3c1f31d523
IBXCODECAT <47277402+Dom733@users.noreply.github.com>
2022-03-12 11:47:23 -0600

    succ

--- app/src/main/java/com/ibxcodecat/frc_scouting/CounterComponent.kt
+++ app/src/main/java/com/ibxcodecat/frc_scouting/CounterComponent.kt
@@ -5,14 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 
 class CounterComponent : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+        //Number manipulation button declarations
+        val makeDecBtn: Button = view.findViewById(R.id.makeDecrementBtn)
+        val makeIncBtn: Button = view.findViewById(R.id.makeIncrementBtn)
+        val missDecBtn: Button = view.findViewById(R.id.missDecrementBtn)
+        val missIncBtn: Button = view.findViewById(R.id.missIncrementBtn)
+        
+        //Variables to be manipulated (just like my ex in my previous relationship)
+        var makeNum: Int = 0
+        var missNum: Int = 0
+
+        //Listeners to manipulate the numbers
+        makeDecBtn.setOnClickListener()
+    }
+
+    override fun onCreateView(inflater : LayoutInflater, container : ViewGroup?,
+                              savedInstanceState: Bundle?): View?{
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_counter_component, container, false)
     }
 
-    val makeDecBtn = findViewById<Button>(R.id.makeDecrementBtn)
+
 }
