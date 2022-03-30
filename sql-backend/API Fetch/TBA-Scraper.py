dad9f14767fa5d0a26ae8e61ab356623cb760276
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-03-29 20:24:32 -0500

    Update TBA-Scraper.py

--- sql-backend/API Fetch/TBA-Scraper.py
+++ sql-backend/API Fetch/TBA-Scraper.py
@@ -3,14 +3,17 @@ import requests
 baseURL = 'http://www.thebluealliance.com/api/v3/'
 header = {'X-TBA-Auth-Key':'0d0KrYpvEPhoi2zN9wskkcfRNYlDRSqxdey21G7XaLWau0BXJYLfKWLLXWsx7BXB'} 
 
-def getTBA(url):
-	return requests.get(baseURL + url, headers=header).json()
-
-event_key = ""
+def getTBA(url): return requests.get(baseURL + url, headers=header).json()
 
 teamsAtChamps = getTBA("event/2022wimi/teams")
+matches = getTBA("event/2022wimi/matches")
 
-for team in teamsAtChamps:
-	#print(team)
-	team_number = team['team_number']
-	print(team_number)
+api_scrapes = [getTBA("event/2022wimi/teams"), getTBA("event/2022wimi/matches")] #Contain all API scrapes in an array
+
+for scrape in api_scrapes: #For each scrape in our set of api scrapes...
+	for data_obj in scrape: #For each data object in our scrape...
+		if 'team_number' in data_obj:
+			print(data_obj['team_number'])
+		elif 'match_number' in data_obj:
+			print(data_obj['match_number'])
+			
