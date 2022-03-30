f3588e5f94c3ef7828f75938d2ff7c03f0f2c3dd
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-03-29 20:50:46 -0500

    Update TBA-Scraper.py

--- sql-backend/API Fetch/TBA-Scraper.py
+++ sql-backend/API Fetch/TBA-Scraper.py
@@ -1,19 +1,17 @@
 import requests
 
 baseURL = 'http://www.thebluealliance.com/api/v3/'
-header = {'X-TBA-Auth-Key':'0d0KrYpvEPhoi2zN9wskkcfRNYlDRSqxdey21G7XaLWau0BXJYLfKWLLXWsx7BXB'} 
+header = {'X-TBA-Auth-Key':'0d0KrYpvEPhoi2zN9wskkcfRNYlDRSqxdey21G7XaLWau0BXJYLfKWLLXWsx7BXB'} #bad api key
 
 def getTBA(url): return requests.get(baseURL + url, headers=header).json()
 
 teamsAtChamps = getTBA("event/2022wimi/teams")
-matches = getTBA("event/2022wimi/matches")
 
-api_scrapes = [getTBA("event/2022wimi/teams"), getTBA("event/2022wimi/matches")] #Contain all API scrapes in an array
+def GetMatchKeys(event_key): #Fetch a list of match keys from an event specified by the event key
+	match_keys = getTBA("event/" + event_key + "/matches/keys")
+	return match_keys
+	
+def GetMatchDataFromKey(match_key): #Fetch match data from a match specified the match key
+	match_data = getTBA()
 
-for scrape in api_scrapes: #For each scrape in our set of api scrapes...
-	for data_obj in scrape: #For each data object in our scrape...
-		if 'team_number' in data_obj:
-			print(data_obj['team_number'])
-		elif 'match_number' in data_obj:
-			print(data_obj['match_number'])
-			
+print(GetMatchKeys("2022wimi"))
