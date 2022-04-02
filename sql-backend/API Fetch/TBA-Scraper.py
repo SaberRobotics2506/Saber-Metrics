b67b3e96116f48c05f2f9908dc7c46059c679c50
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-04-02 12:32:13 -0500

    Update TBA-Scraper.py

--- sql-backend/API Fetch/TBA-Scraper.py
+++ sql-backend/API Fetch/TBA-Scraper.py
@@ -2,44 +2,50 @@ import requests
 
 api_key = ''
 
-with open('api-key.txt') as filestream:
-    api_key = filestream.readlines()
+with open('api-key.txt') as filestream: api_key = filestream.readlines() #Load API_KEY from file
 
-for key in api_key: #For each key in our api_key list (there is only one)
-	api_key = key	#Set our api_key to the extracted key
-
-print(api_key)
+for key in api_key: api_key = key #For each key in our api_key list (there is only one) Set our api_key to the extracted key
 	
-baseURL = 'http://www.thebluealliance.com/api/v3/'
-header = {'X-TBA-Auth-Key':api_key} #bad api key
+baseURL = 'http://www.thebluealliance.com/api/v3/' #The API we will be scraping
+header = {'X-TBA-Auth-Key':api_key} #Create our request header
 
-print(header)
-
+#A magic function that makes a web request. The world may never know what it truly does!
 def getTBA(url): return requests.get(baseURL + url, headers=header).json()
 
 teamsAtChamps = getTBA("event/2022wimi/teams")
 
-def AssembleMatchAPIScrapes(match_keys):
+#Assemble match scrapes from given match keys
+def AssembleMatchScrapes(match_keys):
+	
 	scrapes = []
 	
-	for key in match_keys:
+	for key in match_keys: 
 		scrape = "/match/" + str(key)
 		scrapes.append(scrape)
 		
 	return scrapes
-
-def AssembleMatchKeys(event_key): #Fetch a list of match keys from an event specified by the event key
+	
+#Fetch a list of match keys from an event specified by the event key
+def FetchMatchKeys(event_key):
 	match_keys = getTBA("event/" + event_key + "/matches/keys")
 	return match_keys
 	
-def GetMatchData(match_scrape): #Fetch match data from a match specified the match key
-	match_data = getTBA(match_scrape)
+#Fetch match data by scraping the TBA API for each match
+def FetchMatchData(match_scrapes):
+	
+	match_data = []
+	
+	for scrape in match_scrapes:
+		match_data.append(getTBA(scrape))
+		print(match_data)
+		
 	return match_data
 
-match_keys = AssembleMatchKeys("2022wimi")
-match_scrapes = AssembleMatchAPIScrapes(match_keys)
 
-print(GetMatchData(match_scrapes[0]))
-	
+match_keys = FetchMatchKeys("2022wimi")
+match_scrapes = AssembleMatchScrapes(match_keys)
+match_data = FetchMatchData(match_scrapes)
+
 print(str(match_keys) + " match keys")
 print(str(match_scrapes) + " match scrapes")
+print(str(match_data) + " match data")
